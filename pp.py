e1b09f3470bd567264cf1dca8642efb54cfc8bd4
Boby Tiwari <111265239+Boby900@users.noreply.github.com>
2022-11-12 13:50:48 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,67 +1,49 @@
-import random
-quit = False
-computer_score=0
-your_score=0
+def addition(x,y):
+    print(x+y)
+def substraction(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+def sqroot(x):
+    print(x*x)
 
-while quit == False:
-    choices= ["rock","paper","scissor"]
-    user_input= input("choose rock,paper,scissor or quit\n").lower()
-    computer_input= random.choice(choices)
-    
-    if user_input == "quit":
-        print("you ended the game")
-        print(f"your total score is {your_score}"+" and "+f"computer total score is {computer_score}")
-        quit=True
-        if computer_score < your_score:
-            print("You Won 🏆🏆🏆")
-        elif computer_score> your_score:
-            print("computer won 😭😭")
-        else:
-            print("match tie")
-    elif user_input == "rock":
-        if computer_input == "rock":
-            print("computer input is rock")
-            print("your input is also rock")
-            print("it is tie")
-        elif computer_input =="scissor":
-            print("computer input is scissor")
-            print("your input is rock")
-            print("you won")
-            your_score+=1
-        elif computer_input =="paper":
-            print("computer input is paper")
-            print("your input is rock")
-            print("computer won")
-            computer_score+=1
-    elif user_input == "paper":
-        if computer_input == "paper":
-            print("computer input is paper")
-            print("your input is also paper")
-            print("it is tie")
-        elif computer_input =="scissor":
-            print("computer input is scissor")
-            print("your input is paper")
-            print("computer won")
-            computer_score+=1
-        elif computer_input =="rock":
-            print("computer input is rock")
-            print("your input is paper")
-            print("you won")
-            your_score+=1
-    elif user_input == "scissor":
-        if computer_input == "scissor":
-            print("computer input is scissor")
-            print("your input is also scissor")
-            print("it is tie")
-        elif computer_input =="rock":
-            print("computer input is rock")
-            print("your input is scissor")
-            print("computer won")
-            computer_score+=1
-        elif computer_input =="paper":
-            print("computer input is paper")
-            print("your input is scissor")
-            print("you won")
-            your_score+=1
-    elif user_input != "rock" or user_input !="scissor" or user_input !="paper":
-        print("Invalid input")                
+print("A. addition")
+print("B substraction")
+print("C multiply")
+print("D divide")
+print("S square root")
+print("Q exit")
+while True:
+    choice= input("input your choice 🚀"+"\n")
+
+    if choice == "A" or choice=="a":
+        print("addition"+"\n")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        addition(a,b)
+    elif choice =="B" or choice=="b":
+        print("substraction"+"\n")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        substraction(a,b)
+    elif choice == "C" or choice == "c":
+        print("multiply"+"\n")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        multiply(a,b)
+    elif choice == "D" or choice == "d":
+        print("Divide"+"\n")
+        a = int(input("input first number"))
+        b = int(input("input second number"))
+        divide(a,b)
+    elif choice == "S" or choice == "s":
+        print("squre root"+"\n")
+        a = int(input("input the number to be squared"))
+        sqroot(a)
+
+
+    elif choice == "Q" or choice == "q":
+        print("program ended"+"\n")
+        break
